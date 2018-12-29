99b6f278b01c285aa1f1c63f57e813b7be47cfef
James Seward <james@jamesoff.net>
2018-12-29 21:24:29 +0000

    Adjust number of threads by CPU count

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate num;
 extern crate image;
 extern crate crossbeam;
+extern crate num_cpus;
 
 use num::Complex;
 use image::ColorType;
@@ -129,7 +130,8 @@ fn main() {
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
     // render(&mut pixels, bounds, upper_left, lower_right);
-    let threads = 8;
+    let threads = num_cpus::get();
+    writeln!(std::io::stdout(), "Using {} theads.", threads).unwrap();
     let rows_per_band = bounds.1 / threads + 1;
     {
         let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
